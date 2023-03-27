43f98f481454ba5a4a935bd6d6f95fd9e688a4e1
Anderson <andersonmtv07@gmail.com>
2023-03-27 03:13:40 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+public class Program
+
+{
+    static void Main()
+
+    {
+        string[] entrada = Console.ReadLine().Split();
+        int participantes = int.Parse(entrada[0]);
+        double cachorrosQuentes = int.Parse(entrada[1]);
+        Console.WriteLine($"{(participantes / cachorrosQuentes):F2}");
+    }
+
+}
